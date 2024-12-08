028a8488b61997f19a5ea86bd74eed1a63362046
ye <solanoymf@gmail.com>
2024-12-08 14:16:59 +0100

    done

--- index.php
+++ index.php
@@ -9,16 +9,24 @@ $route = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $routes = [
     '/' => 'index.twig',
     '/landing' => 'landing.twig',
+    '/underConstruction' => 'underConstruction.twig',
     '/generic' => 'generic.twig',
     '/elements' => 'elements.twig',
 ];
 
-if (array_key_exists($route, $routes)) {
+if (
+    !array_key_exists($route, $routes) ||
+    ($route === '/landing' && (!isset($_GET['item']) ||
+    !in_array($_GET['item'], ['eventiverse', 'backoffice'])))
+) {
+    echo $twig->render('404.twig', []);
+}else{
     if ($route === '/landing' && isset($_GET['item'])) {
         echo $twig->render($routes[$route], ['item' => $_GET['item']]);
     } else {
         echo $twig->render($routes[$route]);
     }
-} else {
-    echo $twig->render('errorPage.twig', []);
 }
+
+
+
